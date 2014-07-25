1e356cba9ce22acd881afbde16a67675ee3b1bba
RajdeepPathak <rajdeep.pathak@ascomp.com>
2014-07-25 11:23:27 +0530

    tt
    test data

---
+++ app.config.js
@@ -0,0 +1,69 @@
+window.R3MobileApp = $.extend(true, window.R3MobileApp, {
+  "config": {
+    "layoutSet": "slideout",
+    //"ServiceUrl": "http://localhost:49614/Service1.svc/",
+    "ServiceUrl": "http://192.168.0.171/R3Mobileservice/Service1.svc/",
+    "navigation": [
+      {
+        "title": "Home",
+        "action": "#home",
+        "icon": "home"
+      },
+      {
+        "title": "Customer",
+        "action": "#Customer",
+        "icon": "user"
+      },
+      {
+        "title": "About",
+        "action": "#about",
+        "icon": "info"
+      },
+      {
+        "title": "Orders",
+        "action": "#OrderBrowser",
+        "icon": "browser"
+      },
+      {
+        "title": "New Order",
+        "action": "#OrderEntry",
+        "icon": "plus"
+      },
+      {
+        "title": "Invoice",
+        "action": "#Invoice",
+        "icon": "invoice"
+      },
+      {
+        "title": "New Invoice",
+        "action": "#New_Invoice",
+        "icon": "new invoice"
+      },
+      {
+        "title": "Ledger",
+        "action": "#Ledger",
+        "icon": "ledger"
+      },
+      {
+        "title": "Collection",
+        "action": "#Collection",
+        "icon": "collection"
+      },
+      {
+        "title": "Cart",
+        "action": "#Cart",
+        "icon": "cart"
+      },
+      {
+        "title": "LogIn",
+        "action": "#LogIn",
+        "icon": "login"
+      },      
+      {
+        "title": "TempBrowser1",
+        "action": "#TempBrowser1",
+        "icon": "tempbrowser1"
+      }
+    ]
+  }
+});
